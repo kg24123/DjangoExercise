bdf24b5e79ac0247cd9780c4b8d8a8dc92416592
kg24123 <kg24123@gmail.com>
2017-05-17 17:48:21 +0800

    Fix the error of http response and the url error destination

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&l!2x%0^aenq-oyh*3jk$th-^cdp+$e4rgb5-nv2=uc-i@&c)s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['119.29.241.71']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+   'learnExercise',
 ]
 
 MIDDLEWARE = [
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from learnExercise import views as learn_views # new
 urlpatterns = [
+    url(r'^$',learn_views.index),   #new
     url(r'^admin/', admin.site.urls),
 ]
